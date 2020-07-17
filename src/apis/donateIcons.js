d37d6088e42965c0c6477f0b451a3f8ca08aa3f4
Emmanuel Ola <eoola@wpi.edu>
2020-07-16 22:13:52 -0400

    Updated the donated page with filler text and images. Flex-wrap isn't working as expected, I'll run it with chrome dev-tools to see what's happening.

---
+++ src/apis/donateIcons.js
@@ -0,0 +1,53 @@
+const donateList = [
+    {
+        id: 0,
+        title: "Breonna Taylor",
+        author: "UltraViolet",
+        imgURL: "./20200704_214859.jpg",
+        imgCap: "Testing",
+        link: "https://act.weareultraviolet.org/call/call-for-breonna"
+    },
+    {
+        id: 1,
+        title: "Breonna Taylor",
+        author: "UltraViolet",
+        imgURL: "./20200704_214859.jpg",
+        imgCap: "Testing",
+        link: "https://act.weareultraviolet.org/call/call-for-breonna"
+    },
+    {
+        id: 2,
+        title: "Breonna Taylor",
+        author: "UltraViolet",
+        imgURL: "./20200704_214859.jpg",
+        imgCap: "Testing",
+        link: "https://act.weareultraviolet.org/call/call-for-breonna"
+    },
+    {
+        id: 3,
+        title: "Breonna Taylor",
+        author: "UltraViolet",
+        imgURL: "./20200704_214859.jpg",
+        imgCap: "Testing",
+        link: "https://act.weareultraviolet.org/call/call-for-breonna"
+    },
+    {
+        id: 4,
+        title: "Breonna Taylor",
+        author: "UltraViolet",
+        imgURL: "./20200704_214859.jpg",
+        imgCap: "Testing",
+        link: "https://act.weareultraviolet.org/call/call-for-breonna"
+    },
+    {
+        id: 5,
+        title: "Breonna Taylor",
+        author: "UltraViolet",
+        imgURL: "./20200704_214859.jpg",
+        imgCap: "Testing",
+        link: "https://act.weareultraviolet.org/call/call-for-breonna"
+    },
+
+]
+
+export default donateList
--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Title from '../components/Title';
+import donateList from "../apis/donateIcons";
+import Icon from "../components/Icon";
 
 export default function Donate(){
+    const funds = donateList.map(fund => <Icon title={fund.title} name={fund.author}
+                                                 image={fund.imgURL} alt={fund.imgCap} link={fund.link}
+                                                 imgHeight="200px" imgWidth="100%" />)
+
     return(
         <div style={{height: "100vh", width: "100vw", backgroundColor: "#28272C", color: "#e6e3db"}}>
             <Title title="Donate" subheading="Help with financial support"/>
+            <div
+            style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            flexWrap: "wrap",
+        }}>
+                {funds}
+            </div>
         </div>
     );
 }
