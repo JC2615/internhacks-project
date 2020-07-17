27db9c867f7afb0c9a16f3c9044288f5e225c324
Josh Curry <joshuacurry2615@gmail.com>
2020-07-17 15:58:50 -0500

    Updated listen and resources data to match the information on the google doc

--- src/apis/resourceList.js
+++ src/apis/resourceList.js
@@ -1,82 +1,122 @@
 export const resources = [
     {
         id: 0,
-        heading: "Guide to Allyship",
-        link: null
+        heading: "Steps to Becoming Anti-Racist",
+        link: "https://static1.squarespace.com/static/5b21b436f8370a84045a7256/t/5dfa5d9ccda8467a3e891749/1576689054017/Antiracism+Starter+Kit.pdf"
     },
     {
         id: 1,
-        heading: "10 Steps to Non-Optical Allyship",
-        link: null
+        heading: "Guide to Allyship",
+        link: "https://guidetoallyship.com"
     },
     {
         id: 2,
-        heading: "Guide to Virtual Protesting",
-        link: null
+        heading: "10 Steps to Non-Optical Allyship",
+        link: "https://www.instagram.com/p/CA04VKDAyjb/"
     },
     {
         id: 3,
-        heading: "Natl Resource List #GeorgeFloyd+",
-        link: null
+        heading: "Guide to Virtual Protesting",
+        link: "https://www.instagram.com/p/CAyFgY6jdE0/"
     },
     {
         id: 4,
-        heading: "Talking to Children About Race",
-        link: null
+        heading: "Collective of Black Photographers",
+        link: "https://www.instagram.com/seeinblackproject/"
     },
     {
         id: 5,
-        heading: "14 Black Funds and 23 Creative Ecosystems to Support",
-        link: null
+        heading: "Natl Resource List",
+        link: "https://docs.google.com/document/d/1CjZMORRVuv-I-qo4B0YfmOTqIOa3GUS207t5iuLZmyA/mobilebasic"
     },
     {
         id: 6,
-        heading: "Black Designers + Black-Owned Studios",
-        link: null
+        heading: "Talking to Children About Race",
+        link: "https://www.prettygooddesign.org/blog/Blog%20Post%20Title%20One-5new4?fbclid=IwAR1Xg3lpkvY3t4JVCr8BF9TOa2m-2gdQxkCD_IPSmmopTquWSxbZQa6cX0Q"
     },
     {
         id: 7,
-        heading: "“Social Justice 101” by Erika Hart",
-        link: null
+        heading: "14 Black Funds and 23 Creative Ecosystems to Support",
+        link: "https://www.instagram.com/p/CA-7yLGh3DP/"
     },
     {
         id: 8,
-        heading: "Practical Guide to Protesting & Pandemic Prevention",
-        link: null
+        heading: "Black Designers + Black-Owned Studios",
+        link: "https://docs.google.com/spreadsheets/d/1ZiA-eCUbUoEYsauFgHKoFeWiBL5p9tHj16uYl7jjdtc/edit#gid=0"
     },
     {
         id: 9,
-        heading: "Prompt Letter to LAPD Commissioner",
-        link: null
+        heading: "“Racial and Social Justice 101” by Ericka Hart",
+        link: "https://erickahart.podia.com/racial-and-social-justice-101"
     },
     {
         id: 10,
-        heading: "Legal Aid for Protestors",
-        link: null
+        heading: "Practical Guide to Protesting & Pandemic Prevention",
+        link: "https://www.instagram.com/p/CA4U5oSjCm8/"
     },
     {
         id: 11,
-        heading: "Register to Vote",
-        link: null
+        heading: "Prompt Letter to LAPD Commissioner",
+        link: "https://docs.google.com/document/u/0/d/1_GhoKsxosKNLsUyH9JW7mc8rMEKLb5KXmsDAUPWaWlI/mobilebasic"
     },
     {
         id: 12,
-        heading: "Anti Racist Podcasts",
-        link: null
+        heading: "Legal Aid for Protestors",
+        link: "https://www.instagram.com/p/CA-hCv1J33H/"
     },
     {
         id: 13,
-        heading: "How Kids Learn Prejudice",
-        link: null
+        heading: "Register to Vote",
+        link: "https://vote.gov/"
     },
     {
         id: 14,
         heading: "Home from College's 87 Ways you Can Help",
-        link: null
+        link: "https://www.instagram.com/p/CA32C9vjBj-/"
     },
     {
         id: 15,
         heading: "Red Table Talk: Unpacking White Privilege and Prejudice",
-        link: null
-    }  
+        link: "https://www.facebook.com/redtabletalk/videos/unpacking-white-privilege-and-prejudice/322049678665001/"
+    },
+    {
+        id: 16,
+        heading: "Toolkit for Coping With Racial Trauma",
+        link: "https://medium.com/nappy-head-club/the-four-bodies-a-holistic-toolkit-for-coping-with-racial-trauma-8d15aa55ae06"
+    },
+    {
+        id: 17,
+        heading: "Why the Refusal to Post Online is Often Inherently Racist",
+        link: "https://www.instagram.com/p/CA0zhFzFjLf/?utm_source=ig_web_copy_link"
+    },
+    {
+        id: 18,
+        heading: "Racial Justice Resources",
+        link: "https://docs.google.com/document/u/0/d/14BDM0MHmF9yT2MnOpG7zyy9YSpf4lVfIMeJXUNr8_p8/mobilebasic"
+    },
+    {
+        id: 19,
+        heading: "Defunding the Police and What That Means",
+        link: "https://www.instagram.com/p/CA59D-eFTzE/?igshid=wi2152bzxyt8"
+    },
+    {
+        id: 20,
+        heading: "The 2018 List of Diverse Children's Books",
+        link: "http://hereweeread.com/2017/11/2018-ultimate-list-diverse-childrens-books.html"
+    },
+    {
+        id: 21,
+        heading: "List of Anti-Racism Resources by Dazed",
+        link: "https://www.dazeddigital.com/politics/article/49420/1/a-running-list-of-anti-racism-resources-protest-police-brutality-george-floyd"
+    },  
+    {
+        id: 22,
+        heading: "For Protestors",
+        link: "https://blacklivesmatters.carrd.co/#protesters"
+    },  
+    {
+        id: 23,
+        heading: "Self-Care Tips for Black People Who Are Struggling by Vice",
+        link: "https://www.vice.com/en_us/article/g5pgmq/self-care-tips-for-black-people-struggling-from-painful-week"
+    },    
 ]
