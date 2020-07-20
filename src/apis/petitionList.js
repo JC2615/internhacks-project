3b51235034bd6d60249f6c0fdc42ba28d6833e1d
Emmanuel Ola <eoola@wpi.edu>
2020-07-20 14:32:26 -0400

    Finished the petition page according to design specs

---
+++ src/apis/petitionList.js
@@ -0,0 +1,134 @@
+import React from 'react';
+
+export const petitionsList = [
+    {
+        id: 0,
+        heading: 'Justice for George Floyd',
+        link: "https://petitions.whitehouse.gov/petition/justice-george-floyd-0"
+    },
+    {
+        id: 1,
+        heading: 'Change Police Restraining Methods',
+        link: "https://petitions.whitehouse.gov/petition/police-restraining-methods-need-be-changed-more-innocent-people-are-murdered"
+    },
+    {
+        id: 2,
+        heading: 'Justice for Breonna Taylor',
+        link: "https://www.change.org/p/andy-beshear-justice-for-breonna-taylor/psf/share"
+    },
+    {
+        id: 3,
+        heading: '#JusticeForBre',
+        link: "https://act.colorofchange.org/sign/justiceforbre-breonna-taylor-officers-fired?source=coc_main_website"
+    },
+    {
+        id: 4,
+        heading: 'Tell Congress to protect Black-owned businesses',
+        link: "https://act.colorofchange.org/sign/blackbusiness-congress"
+    },
+    {
+        id: 5,
+        heading: '#RunWithMaud',
+        link: "https://www.runwithmaud.com"
+    },
+    {
+        id: 6,
+        heading: 'Justice For Ahmaud Arbery ',
+        link: "https://www.change.org/p/liberty-county-distric-attorney-justice-for-ahmaud-arbery"
+    },
+    {
+        id: 7,
+        heading: 'Justice for Tony McDade',
+        link: "https://www.change.org/p/black-lives-matter-activists-justice-for-tony-mcdade?recruiter=272924036&utm_source=share_petition&utm_medium=twitter&utm_campaign=psf_combo_share_initial&utm_term=psf_combo_share_initial&recruited_by_id=599cec50-dd86-11e4-ae86-af5c49b42ec1"
+    },
+    {
+        id: 8,
+        heading: 'Pardon Black Woman Imprisoned for Voting',
+        link: "https://www.change.org/p/greg-abbott-pardon-black-woman-imprisoned-for-voting?utm_content=cl_sharecopy_13513769_en-US%3Av1&recruiter=319353471&recruited_by_id=129af720-1490-11e5-9c6e-8515d331dad3&utm_source=share_petition&utm_medium=copylink&utm_campaign=psf_combo_share_initial&utm_term=50bd38eb3cd647ea9681819861c8f293"
+    },
+    {
+        id: 9,
+        heading: 'Justice for Regis Fund',
+        link: "https://ca.gofundme.com/f/cggys2-justice-for-regis"
+    },
+    {
+        id: 10,
+        heading: 'Classify White Supremacist Groups as Domestic Terrorist Orgs',
+        link: "https://www.change.org/p/clare-linkins-executive-director-of-national-counterterrorism-center-lift-the-hoods-classify-white-supremacist-groups-as-domestic-terrorist-organizations?utm_content=cl_sharecopy_22547593_en-US%3Av7&recruiter=664983596&utm_source=share_petition&utm_medium=copylink&utm_campaign=share_petition&utm_term=share_petition"
+    },
+    {
+        id: 11,
+        heading: 'Ban or Restrict Tear Gas',
+        link: "https://www.change.org/p/ban-or-restrict-tear-gas?utm_source=share_petition&utm_medium=custom_url&recruited_by_id=3632b700-e6e6-11e8-9dab-7b2bccf01f79"
+    },
+    {
+        id: 12,
+        heading: 'Justice for Shukri Abdi',
+        link: "https://www.change.org/p/independent-investigation-into-bullying-policy-at-broadoak-sports-college-hazel-wood-high-school-bury-lancashire-and-gmp-full-investigation?utm_content=cl_sharecopy_16441869_en-GB%3Av4&recruited_by_id=1dafb0b0-9c54-11e9-9915-07e890b1330d&utm_source=share_petition&utm_medium=copylink&utm_campaign=psf_combo_share_initial&utm_term=psf_combo_share_initial&share_bandit_exp=initial-16441869-en-GB&share_bandit_var=v3"
+    },
+    {
+        id: 13,
+        heading: 'Stop Neighborhood Oil Drilling',
+        link: "https://www.patagonia.com/actionworks/campaigns/stop-neighborhood-drilling/"
+    },
+    {
+        id: 14,
+        heading: 'Hands Up Act',
+        link: "https://www.change.org/p/us-senate-hands-up-act"
+    },
+    {
+        id: 15,
+        heading: 'End Police Brutality',
+        link: "https://secure.avaaz.org/en/community_petitions/us_government_end_police_brutalitychange_the_law/"
+    },
+    {
+        id: 16,
+        heading: 'Fire Racist Criminal Michael J Reynolds from the NYPD',
+        link: "https://www.change.org/p/nypd-fire-racist-criminal-michael-j-reynolds-from-the-nypd?recruiter=1030005508&utm_source=share_petition&utm_medium=twitter&utm_campaign=psf_combo_share_abi&utm_term=psf_combo_share_initial&recruited_by_id=e0484360-22a2-11ea-8ffd-a199378defb8"
+    },
+    {
+        id: 17,
+        heading: 'Demand Racial Data on Coronavirus',
+        link: "https://blacklivesmatter.com/petitions/"
+    },
+    {
+        id: 18,
+        heading: 'Consequences for excessive force used on Quentin Suttles',
+        link: "https://www.change.org/p/john-j-flynn-consequences-for-excessive-force-used-on-quentin-suttles?utm_content=cl_sharecopy_22133703_en-US%3Av1&recruiter=319353471&recruited_by_id=129af720-1490-11e5-9c6e-8515d331dad3&utm_source=share_petition&utm_medium=copylink&utm_campaign=psf_combo_share_initial&utm_term=psf_combo_share_abi"
+    },
+    {
+        id: 19,
+        heading: 'Free Matthew Rushin',
+        link: "https://www.change.org/p/ralph-s-northam-matthew-rushin-autistic-college-student-odu?utm_content=cl_sharecopy_20037128_en-US%3Av1&recruiter=1038470796&recruited_by_id=de514810-41e0-11ea-95a7-7778a9bc07ed&utm_source=share_petition&utm_medium=copylink&utm_campaign=psf_combo_share_initial&utm_term=psf_combo_share_abi"
+    },
+    {
+        id: 20,
+        heading: 'Reopen Sandra Bland\'s Case',
+        link: "https://www.change.org/p/texas-governor-i-want-sandra-bland-s-case-reopened?recruiter=false&utm_source=share_petition&utm_medium=twitter&utm_campaign=psf_combo_share_initial&utm_term=psf_combo_share_initial&recruited_by_id=456cffa0-a51c-11ea-9ac6-9d6633105c04"
+    },
+    {
+        id: 21,
+        heading: 'Justice for Jamee Johnson',
+        link: "https://campaigns.organizefor.org/petitions/justice-for-jamee-johnson?bucket=&source=twitter-share-button&utm_campaign=&utm_source=twitter&share=f5385b3b-b7d1-47b5-afd3-079382a87ca2"
+    },
+    {
+        id: 22,
+        heading: 'Willie Simmons has served 38 years for a $9 robbery',
+        link: "https://www.change.org/p/alabama-governor-kay-ivey-willie-simmons-has-served-38-years-for-a-9-robbery?utm_content=cl_sharecopy_19727323_en-US%3Av3&recruiter=319353471&recruited_by_id=129af720-1490-11e5-9c6e-8515d331dad3&utm_source=share_petition&utm_medium=copylink&utm_campaign=psf_combo_share_initial&utm_term=psf_combo_share_abi"
+    },
+    {
+        id: 23,
+        heading: 'Ban the use of rubber bullets',
+        link: "https://www.change.org/p/un-and-us-government-ban-the-use-of-rubber-bullets?recruiter=922712485&recruited_by_id=62d8f120-fdb6-11e8-8dbd-0dee42fd9442&utm_source=share_petition&utm_medium=copylink&utm_campaign=petition_dashboard"
+    },
+    {
+        id: 24,
+        heading: '2020 Voter Justice Agenda',
+        link: "https://act.colorofchange.org/sign/heroesactpetition-congress"
+    },
+    {
+        id: 25,
+        heading: 'More Petitions',
+        link: "https://blacklivesmatters.carrd.co/#petitions"
+    }
+]
