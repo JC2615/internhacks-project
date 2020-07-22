3e0f4f692766a58c4809fa8c711b4dc404eecf78
A. S. Young <24947040+a-s-young@users.noreply.github.com>
2020-07-21 20:40:37 -0600

    Added content to history page

--- src/apis/historyEntries.js
+++ src/apis/historyEntries.js
@@ -1,25 +1,64 @@
 const blackHistoryData = [
   {
     id: 0,
-    title: "Tuskeegee Syphilis Experiment",
-    info: "The Tuskegee Study of Untreated Syphilis in the African American Male was a clinical study conducted between 1932 and 1972 by the United States Public Health Service.The Tuskegee Study of Untreated Syphilis in the African American Male was a clinical study conducted between 1932 and 1972 by the United States Public Health Service. The purpose of this study was to observe the natural history of untreated syphilis; the African-American men in the study were only told they were receiving free health care from the Federal government of the United States."
+    title: "Harlem Renaissance",
+    info: "The Harlem Renaissance was the development of the Harlem neighborhood in New York City as a black cultural mecca in the early 20th Century and the subsequent social and artistic explosion that resulted. Lasting roughly from the 1910s through the mid-1930s, the period is considered a golden age in African American culture."
   },
   {
     id: 1,
-    title: "Tuskeegee Syphilis Experiment",
-    info: "The Tuskegee Study of Untreated Syphilis in the African American Male was a clinical study conducted between 1932 and 1972 by the United States Public Health Service.The Tuskegee Study of Untreated Syphilis in the African American Male was a clinical study conducted between 1932 and 1972 by the United States Public Health Service. The purpose of this study was to observe the natural history of untreated syphilis; the African-American men in the study were only told they were receiving free health care from the Federal government of the United States."
+    title: "George Washington Carver",
+    info: "George Washington Carver was an African American scientist and educator. Carver developed 300 derivative products from peanuts among them cheese, milk, coffee, flour, ink, dyes, plastics, wood stains, soap, linoleum, medicinal oils and cosmetics."
   },
   {
     id: 2,
-    title: "Tuskeegee Syphilis Experiment",
-    info: "The Tuskegee Study of Untreated Syphilis in the African American Male was a clinical study conducted between 1932 and 1972 by the United States Public Health Service.The Tuskegee Study of Untreated Syphilis in the African American Male was a clinical study conducted between 1932 and 1972 by the United States Public Health Service. The purpose of this study was to observe the natural history of untreated syphilis; the African-American men in the study were only told they were receiving free health care from the Federal government of the United States."
+    title: "Tuskegee Airmen",
+    info: "The Tuskegee Airmen were the first black military aviators in the U.S. Army Air Corps (AAC), a precursor of the U.S. Air Force. Trained at the Tuskegee Army Air Field in Alabama, they flew more than 15,000 individual sorties in Europe and North Africa during World War II. Their impressive performance earned them more than 150 Distinguished Flying Crosses, and helped encourage the eventual integration of the U.S. armed forces."
   },
   {
     id: 3,
-    title: "Tuskeegee Syphilis Experiment",
-    info: "The Tuskegee Study of Untreated Syphilis in the African American Male was a clinical study conducted between 1932 and 1972 by the United States Public Health Service.The Tuskegee Study of Untreated Syphilis in the African American Male was a clinical study conducted between 1932 and 1972 by the United States Public Health Service. The purpose of this study was to observe the natural history of untreated syphilis; the African-American men in the study were only told they were receiving free health care from the Federal government of the United States."
+    title: "Carter G. Woodson",
+    info: "Carter G. Woodson was an African American writer and historian known as the 'Father of Black History.' He penned the influential book 'The Mis-Education of the Negro.'"
   },
-
+  {
+    id: 4,
+    title: "Jackie Robinson",
+    info: "Jackie Robinson was an African American professional baseball player who broke Major Leagues Baseball’s infamous “color barrier” when he started at first base for the Brooklyn Dodgers on April 15, 1947. Until that time, professional ballplayers of color suited up for teams only in the Negro Leagues."
+  },
+  {
+    id: 5,
+    title: "Brown v. Board Of Education",
+    info: "Brown v. Board of Education of Topekawas a landmark 1954 Supreme Court case in which the justices ruled unanimously that racial segregation of children in public schools was unconstitutional. Brown v. Board of Education was one of the cornerstones of the civil rights movement, and helped establish the precedent that “separate-but-equal” education and other services were not, in fact, equal at all."
+  },
+  {
+    id: 6,
+    title: "Birmingham Church Bombed",
+    info: "The Birmingham church bombing occurred on September 15, 1963, when a bomb exploded before Sunday morning services at the 16th Street Baptist Church in Birmingham, Alabama—a church with a predominantly black congregation that also served as a meeting place for civil rights leaders. Four young girls were killed and many other people injured. Outrage over the incident and the violent clash between protesters and police that followed helped draw national attention to the hard-fought, often-dangerous struggle for civil rights for African Americans."
+  },
+  {
+    id: 7,
+    title: "Selma to Montgomery March",
+    info: "The Selma to Montgomery march was part of a series of civil rights protests that occurred in 1965 in Alabama, a Southern state with deeply entrenched racist policies. The historic march, and Martin Luther King, Jr.’s participation in it, raised awareness of the difficulties faced by black voters, and the need for a national Voting Rights Act."
+  },
+  {
+    id: 8,
+    title: "Thurgood Marshall",
+    info: "Thurgood Marshall — perhaps best known as the first African-American Supreme Court justice — played an instrumental role in promoting racial equality during the civil rights movement. Most historians regard him as an influential figure in shaping social policies and upholding laws to protect minorities."
+  },
+  {
+    id: 9,
+    title: "Fair Housing Act",
+    info: "The Fair Housing Act of 1968 prohibited discrimination concerning the sale, rental and financing of housing based on race, religion, national origin or sex. The Fair Housing Act stands as the final great legislative achievement of the civil rights era."
+  },
+  {
+    id: 10,
+    title: "Los Angeles Riots",
+    info: "The Los Angeles riots sprung from years of rising tensions between the LAPD and the city’s African Americans, highlighted by the 1991 videotaped beating of motorist Rodney King. On April 29, 1992, anger boiled over after four LAPD officers were found not guilty of assaulting King, leading to several days of widespread violence, looting and arson throughout L.A. By May 3, thousands of National Guardsmen and federal troops had largely curbed the uprising, which left more than 60 people dead and produced about $1 billion in damage."
+  },
+  {
+    id: 11,
+    title: "Black History Milestones: A Timeline",
+    info: ""
+  },
 ]
 
 export default blackHistoryData
